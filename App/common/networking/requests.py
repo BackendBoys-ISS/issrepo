2a7a08f39992f635e7b1ce79ecad48a310f2c5b2
andreibanu99 <banuandrei99@gmail.com>
2020-05-05 19:11:56 +0300

    123

--- App/common/networking/requests.py
+++ App/common/networking/requests.py
@@ -25,3 +25,14 @@ class Echo(Request):
 
     def __str__(self):
         return 'Echo: {}'.format(self.text)
+
+class ReviewerShit(Request):
+
+    def __init__(self, reviewer_id):
+        self.reviewer_id = reviewer_id
+        # paper_1 and paper_2 can be objects as long as the client has access to them
+        self.paper_1 = None # set by server
+        self.paper_2 = None # set by server
+
+    def get_reviewer_id(self):
+        return self.reviewer_id
